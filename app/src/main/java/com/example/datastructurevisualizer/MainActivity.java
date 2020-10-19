7af7c3361a1a5a2121eb73ee8bd11923fff3a3ad
Allison Winter-Reed <38668004+AllisonWinter-Reed@users.noreply.github.com>
2020-10-18 21:52:30 -0500

    Main Activity Canvas setup to begin working on Visualizer

--- app/src/main/java/com/example/datastructurevisualizer/MainActivity.java
+++ app/src/main/java/com/example/datastructurevisualizer/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.datastructurevisualizer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
+    private Canvas canvas;
+    private ViewAnimator viewAnimator;
+    private ImageView drawImage;
+    private Paint paint;
+    private final int OFFSET = 120;
+    private int mOffset = OFFSET;
+    private Bitmap bitmap;
+    private int vWidth;
+    private int vHeight;
+    private int circleOffset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawImage = findViewById(R.id.animatorImage);
+        paint = new Paint();
+        paint.setColor(Color.RED);
+    }
+
+
+    /**
+     * Method used to test the functionality of Canvas. Draws a circle when the button is pressed.
+     * TODO delete this
+     */
+    public void drawRedCircle(View view) {
+        paint.setColor(Color.RED);
+        canvas.drawCircle(300f + circleOffset,300f + circleOffset,100f, paint);
+        circleOffset += 20;
+
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        vWidth = drawImage.getWidth();
+        vHeight = drawImage.getHeight();
+
+        bitmap = Bitmap.createBitmap(vWidth, vHeight, Bitmap.Config.ARGB_8888);
+        drawImage.setImageBitmap(bitmap);
+        canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.BLACK);
+    }
+
+
+    /**
+     * Method used to test the functionality of Canvas. Draws a circle when the button is pressed.
+     * TODO delete this
+     */
+    public void drawBlueCircle(View view) {
+
+        Canvas canvas2 = getCanvas();
+
+        paint.setColor(Color.BLUE);
+        canvas2.drawCircle(300f + circleOffset,300f + circleOffset,100f, paint);
+        circleOffset += 20;
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
     }
 }
