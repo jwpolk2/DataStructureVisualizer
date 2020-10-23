cde443a0a69a64a1b276a78bd263e9986ecae161
Jessica <jmiyake@wisc.edu>
2020-10-23 15:28:57 -0500

    added dialog_file_action.xml and dialog_save.xml, started DialogFileAction.java

---
+++ app/src/main/java/com/example/datastructurevisualizer/ui/DialogFileAction.java
@@ -0,0 +1,16 @@
+package com.example.datastructurevisualizer.ui;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class DialogFileAction extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
