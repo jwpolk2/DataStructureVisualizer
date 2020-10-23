bfb92605f91507e250b2ea25ac0621e0ad132ff5
Milan-Tomic <mtomic@wisc.edu>
2020-10-23 13:59:44 -0500

    Added text colour to animation parameters.

--- app/src/main/java/com/example/datastructurevisualizer/AnimationParameters.java
+++ app/src/main/java/com/example/datastructurevisualizer/AnimationParameters.java
@@ -8,10 +8,18 @@ public class AnimationParameters {
     // Default time taken for any given animation.
     static final int ANIM_TIME = 1000;
 
+    // Default number of steps for any Node movement animation.
+    static final int MOVEMENT_FRAMES = 20;
+
     // Default colour of a vector.
     static final int VEC_R = 0;
     static final int VEC_G = 0;
     static final int VEC_B = 0;
+
+    // Default colour of text.
+    static final int TEXT_R = 0;
+    static final int TEXT_G = 0;
+    static final int TEXT_B = 0;
 
     // Current position in the Canvas.
     static float xPos = 0;
