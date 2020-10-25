4dc75c8d8bc897541b55fc7a2111bd2e08ba47f5
Samraaj Thackeray <samrajig@gmail.com>
2020-10-25 14:39:07 -0400

    Modified DrawNode

--- app/src/main/java/com/example/datastructurevisualizer/NodeVisualizer.java
+++ app/src/main/java/com/example/datastructurevisualizer/NodeVisualizer.java
@@ -1,4 +1,5 @@
 package com.example.datastructurevisualizer;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.Log;
 
@@ -29,13 +30,13 @@ public class NodeVisualizer {
         // Draws the Node.
         colour.setARGB(255, node.r, node.g, node.b);
         Paint textPaint = new Paint();
-        textPaint.setARGB(255, 255, 255,255);
+        textPaint.setColor(Color.WHITE);
         textPaint.setTextAlign(Paint.Align.CENTER);
-        textPaint.setTextSize(18 * AnimationParameters.scaleFactor);
+        textPaint.setTextSize(40);
         MainActivity.getCanvas().drawCircle(
                 node.position[0], node.position[1],
                 NODE_WIDTH * AnimationParameters.scaleFactor, colour);
-        MainActivity.getCanvas().drawText(String.valueOf(node.key), node.position[0], node.position[1], textPaint);
+        MainActivity.getCanvas().drawText(String.valueOf(node.key), node.position[0], node.position[1]+15, textPaint);
     }
 
     /**
