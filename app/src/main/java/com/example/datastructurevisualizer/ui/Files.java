564c9e44aea94aaf7bcf381377f79d0826a5d24f
Jessica <jmiyake@wisc.edu>
2020-10-27 12:50:31 -0500

    file frag debug button

--- app/src/main/java/com/example/datastructurevisualizer/ui/Files.java
+++ app/src/main/java/com/example/datastructurevisualizer/ui/Files.java
@@ -1,9 +1,13 @@
 package com.example.datastructurevisualizer.ui;
 
+import android.app.Dialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
@@ -15,8 +19,8 @@ import com.example.datastructurevisualizer.R;
  * Use the {@link Files#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Files extends Fragment {
-
+public class Files extends Fragment implements DialogFileAction.DialogFileActionListener{
+    private Button testButton;
     public Files() {
         // Required empty public constructor
     }
@@ -34,6 +38,20 @@ public class Files extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_files, null);
+        testButton = (Button) view.findViewById(R.id.fileDialogTestBtn);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialog();
+            }
+        });
+    }
+
+    public void openDialog() {
+        DialogFileAction dialogFileAction = new DialogFileAction();
+        dialogFileAction.show(getFragmentManager(), "file action dialog");
     }
 
     @Override
