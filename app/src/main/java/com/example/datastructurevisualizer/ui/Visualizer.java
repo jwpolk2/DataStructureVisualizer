8cf99b47604f30eb2a0ed92da4721f256910aa56
Jessica <jmiyake@wisc.edu>
2020-10-30 11:34:32 -0500

    changed toast for null node insert

--- app/src/main/java/com/example/datastructurevisualizer/ui/Visualizer.java
+++ app/src/main/java/com/example/datastructurevisualizer/ui/Visualizer.java
@@ -119,7 +119,7 @@ public class Visualizer extends Fragment {
             @Override
             public void onClick(View v) {
                 if (insertNumber.getText().length() == 0) {
-                    Toast.makeText(getActivity(), "Bruh", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getActivity(), "Please insert a value", Toast.LENGTH_SHORT).show();
 
                 }
                 else {
