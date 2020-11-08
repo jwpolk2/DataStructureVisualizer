46714f957bf3145313bacd60a5e358e0197842c6
Allison Winter-Reed <38668004+allisonwinter-reed@users.noreply.github.com>
2020-11-08 16:31:41 -0600

    Buttons on Graph Page

--- app/src/main/java/com/example/datastructurevisualizer/ui/GraphVisualizer.java
+++ app/src/main/java/com/example/datastructurevisualizer/ui/GraphVisualizer.java
@@ -7,12 +7,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.datastructurevisualizer.MainActivity;
 import com.example.datastructurevisualizer.R;
 
 
 public class GraphVisualizer extends Fragment {
+    private Button display;
+    private Button button2;
 
 
     public GraphVisualizer() {
@@ -31,7 +35,28 @@ public class GraphVisualizer extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_graph_visualizer, container, false);
         MainActivity.actionBar.setTitle("Graph");
-        MainActivity.actionBar.setDisplayHomeAsUpEnabled(true);
+
+
+        display = view.findViewById(R.id.display_button);
+        button2 = view.findViewById(R.id.button2_button);
+
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Display Button Pressed", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Button 2 Pressed", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+
         return view;
     }
 }
