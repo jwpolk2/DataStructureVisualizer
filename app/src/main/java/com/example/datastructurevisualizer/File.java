d087c86cea4cd9e5acb2c13e8da1dd2501e96653
Jessica <jmiyake@wisc.edu>
2020-11-08 20:29:01 -0600

    implemented file class and adapter

---
+++ app/src/main/java/com/example/datastructurevisualizer/File.java
@@ -0,0 +1,58 @@
+package com.example.datastructurevisualizer;
+
+import java.util.ArrayList;
+
+public class File {
+    private String fileName;
+    private String structureType; // can be an int if easier
+    private String dateModified;
+    private ArrayList<Integer> values;
+
+    public File() {
+        values = new ArrayList<>();
+    }
+
+    public File(ArrayList<Integer> values) {
+        this.values = values;
+    }
+
+    public File(String name, String type, String date) {
+        values = new ArrayList<Integer>();
+        fileName = name;
+        structureType = type;
+        dateModified = date;
+    }
+
+    public ArrayList<Integer> getValues() {
+        return values;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getStructureType() {
+        return structureType;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+
+    public void setValues(ArrayList<Integer> values) {
+        this.values = values;
+    }
+
+    public void setFileName(String name) {
+        fileName = name;
+    }
+
+    public void setStructureType(String type) {
+        structureType = type;
+    }
+
+    public void setDate(String date) {
+        dateModified = date;
+    }
+}
