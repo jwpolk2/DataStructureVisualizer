da977af50c07a241398d39c5e39cbdb6f9be6e8d
jwpolk2 <jwpolk2@wisc.edu>
2020-11-13 19:23:18 -0600

    "Commented out hash and implemented Files tests"

---
+++ app/src/test/java/com/example/datastructurevisualizer/FileTest.java
@@ -0,0 +1,64 @@
+package com.example.datastructurevisualizer;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileTest {
+    String fileName = "name";
+    String type = "Binary Search Tree";
+    String date = "11-1-2020";
+    ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+
+    File file1 = new File(fileName, type, date);
+
+    @Test
+    void setValues() {
+        file1.setValues(numbers);
+        assertEquals(numbers, file1.getValues());
+    }
+
+    @Test
+    void getValues() {
+        ArrayList<Integer> valEmpty = new ArrayList<>();
+        assertEquals(valEmpty, file1.getValues());
+    }
+
+    @Test
+    void getFileName() {
+        assertEquals(fileName, file1.getFileName());
+    }
+
+    @Test
+    void getStructureType() {
+        assertEquals(type, file1.getStructureType());
+    }
+
+    @Test
+    void getDateModified() {
+        assertEquals(date, file1.getDateModified());
+    }
+
+
+    @Test
+    void setFileName() {
+        file1.setFileName("testName");
+        assertEquals("testName", file1.getFileName());
+    }
+
+    @Test
+    void setStructureType() {
+        file1.setStructureType("hashMap");
+        assertEquals("hashMap", file1.getStructureType());
+    }
+
+    @Test
+    void setDate() {
+        file1.setDate("7-7-2020");
+        assertEquals("7-7-2020", file1.getDateModified());
+    }
+}
