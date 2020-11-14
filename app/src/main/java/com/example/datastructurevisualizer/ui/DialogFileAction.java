c5d96f5021da9cbf52c43363ea8c4765d9dd5454
jwpolk2 <jwpolk2@wisc.edu>
2020-11-13 18:31:30 -0600

    "Further implemented save files recycler view"

--- app/src/main/java/com/example/datastructurevisualizer/ui/DialogFileAction.java
+++ app/src/main/java/com/example/datastructurevisualizer/ui/DialogFileAction.java
@@ -1,35 +1,22 @@
 package com.example.datastructurevisualizer.ui;
-import android.content.Context;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
-import org.json.JSONObject;
-import org.json.JSONArray;
-import org.json.JSONException;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.datastructurevisualizer.R;
-import com.example.datastructurevisualizer.TreeVisualizer;
-import com.example.datastructurevisualizer.VisualizerCanvas;
-import com.example.datastructurevisualizer.MainActivity;
+
+import java.util.ArrayList;
 
 //public class DialogFileAction extends AppCompatDialogFragment {
 //    @NonNull
@@ -62,109 +49,48 @@ import com.example.datastructurevisualizer.MainActivity;
 //}
 
 public class DialogFileAction extends DialogFragment {
-    private TextView fileName;
     private Button renameBtn;
     private Button loadBtn;
     private Button deleteBtn;
     private Button cancelBtn;
-    private String mFile;
 
-    public void setTree(TreeVisualizer tree) {
-        this.tree = tree;
+    public void setVals(ArrayList<Integer> vals) {
+        this.vals = vals;
     }
 
-    private TreeVisualizer tree;
-
-    public void setVisualizerCanvas(VisualizerCanvas visualizerCanvas) {
-        this.visualizerCanvas = visualizerCanvas;
+    public void setDsType(String dsType) {
+        this.dsType = dsType;
     }
-    private VisualizerCanvas visualizerCanvas;
+
+    private ArrayList<Integer> vals;
+    private String dsType;
+
     public DialogFileAction() {
 
     }
 
-    public static DialogFileAction newInstance(String file) {
+    public static DialogFileAction newInstance(String title) {
         DialogFileAction fileActionDialog = new DialogFileAction();
         Bundle args = new Bundle();
-        args.putString("file",file);
+        args.putString("save",title);
         fileActionDialog.setArguments(args);
         return fileActionDialog;
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mFile = getArguments().getString("file");
         View view = inflater.inflate(R.layout.dialog_file_action, container);
-        fileName = view.findViewById(R.id.fileDialog_fileName);
-        fileName.setText(mFile);
         renameBtn = view.findViewById(R.id.fileDialog_renameBtn);
         loadBtn = view.findViewById(R.id.fileDialog_loadBtn);
         deleteBtn = view.findViewById(R.id.fileDialog_deleteBtn);
         cancelBtn = view.findViewById(R.id.fileDialog_cancelBtn);
 
-        renameBtn.setOnClickListener(new View.OnClickListener() {
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dismiss();
             }
         });
         loadBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Context context = getContext();
-                try {
-                    //read from file into a JSON format string
-                    Log.i("Read from", context.getFilesDir().toString());
-                    File file = new File(context.getFilesDir(), mFile);
-                    FileReader fileReader = new FileReader(file);
-                    BufferedReader bufferedReader = new BufferedReader(fileReader);
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line = bufferedReader.readLine();
-                    while (line != null) {
-                        stringBuilder.append(line).append("\n");
-                        line = bufferedReader.readLine();
-                    }
-                    bufferedReader.close();
-                    // This response will have Json Format String
-                    String response = stringBuilder.toString();
-
-                    //parse string to JSONObject
-                    JSONObject obj = new JSONObject(response);
-
-                    //get json array of values from the object
-                    JSONArray array = obj.optJSONArray("Values");
-
-                    // Deal with the case of a non-array value.
-                    if (array == null) {
-                        return;
-                    }
-
-                    //remove all the nodes from the tree if there are any so the new ones can be loaded in
-                    tree.clear();
-                    if (visualizerCanvas.canvas == null) {
-                        int vHeight = visualizerCanvas.getHeight();
-                        int vWidth = visualizerCanvas.getWidth();
-                        visualizerCanvas.setDimensions(vHeight, vWidth);
-                    }
-
-                    // Insert numbers from JSONArray into the tree.
-                    ArrayList<Integer> arr = new ArrayList<Integer>();
-                    for (int i = 0; i < array.length(); ++i) arr.add(array.optInt(i));
-                    tree.insert(arr);
-
-                }
-                catch(IOException | JSONException e){
-                    Log.e("Exception", "File read failed: " + e.toString());
-                }
-            }
-        });
-        deleteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dismiss();
--- app/src/main/java/com/example/datastructurevisualizer/ui/Files.java
+++ app/src/main/java/com/example/datastructurevisualizer/ui/Files.java
@@ -4,12 +4,15 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.app.Dialog;
+
 import androidx.appcompat.app.AppCompatDialogFragment;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +27,13 @@ import com.example.datastructurevisualizer.File;
 import com.example.datastructurevisualizer.MainActivity;
 import com.example.datastructurevisualizer.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -39,6 +49,7 @@ public class Files extends Fragment {
     private RecyclerView fileRecyclerView;
     private FileAdapter fileAdapter;
     private RecyclerView.LayoutManager fileLayoutManager;
+
     public Files() {
         // Required empty public constructor
     }
@@ -58,38 +69,15 @@ public class Files extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       final View view = inflater.inflate(R.layout.fragment_files, container, false);
-       Context context = getContext();
-       com.example.datastructurevisualizer.File file = new com.example.datastructurevisualizer.File();
-        com.example.datastructurevisualizer.File file2 = new com.example.datastructurevisualizer.File();
+        java.io.File[] jsonFiles = extractFiles();
         ArrayList<File> mFile = new ArrayList<File>();
-        ArrayList<Integer> values = new ArrayList<>();
-        values.add(2);
-        values.add(3);
-        values.add(5);
-        file.setValues(values);
-        file.setFileName("Joe's File");
-        file.setStructureType("Binary Search Tree");
-        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
-        Date date = new Date();
-        file.setDate(dateFormat.format(date));
-        file2.setFileName("File 2");
-        file2.setStructureType("Binary Search Tree");
-        file2.setDate(dateFormat.format(date));
-        mFile.add(file);
-        mFile.add(file2);
-       // testButton = view.findViewById(R.id.fileDialogTestBtn);
-//        testButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //TODO Was shutting down my application
-////                DialogFragment dialogFileAction = new DialogFileAction();
-////                FragmentTransaction ft = getFragmentManager().beginTransaction();
-////                MainActivity.openDialog(dialogFileAction, "file_action", ft);
-//            }
-//        });
+        mFile = tomFile(jsonFiles);
+        final View view = inflater.inflate(R.layout.fragment_files, container, false);
+        Context context = getContext();
+        com.example.datastructurevisualizer.File file = new com.example.datastructurevisualizer.File();
+        com.example.datastructurevisualizer.File file2 = new com.example.datastructurevisualizer.File();
 
-        //final View view = inflater.inflate(R.layout.fragment_manage_apiaries, container, false);
+
         fileRecyclerView = view.findViewById(R.id.filesRecycle);
         fileRecyclerView.setHasFixedSize(true);
         fileLayoutManager = new LinearLayoutManager(getActivity());
@@ -105,9 +93,11 @@ public class Files extends Fragment {
                 Fragment prev = getFragmentManager().findFragmentByTag("File Action");
                 if (prev != null) {
                     ft.remove(prev);
-                } ft.addToBackStack(null);
+                }
+                ft.addToBackStack(null);
 
                 DialogFileAction fileActionDialog = new DialogFileAction();
+                //fileActionDialog.setVals(fileAdapter);
 //                saveDialog.setTree(tree);
 //                saveDialog.setDataStructureType(dataStructureType);
                 fileActionDialog.show(ft, "File Action");
@@ -117,5 +107,100 @@ public class Files extends Fragment {
         MainActivity.actionBar.setDisplayHomeAsUpEnabled(true);
         MainActivity.actionBar.show();
         return view;
+    }
+
+    /**
+     * open up the files directory and place all saved data structure files into an array
+     *
+     * @return an array of saved files
+     */
+    public java.io.File[] extractFiles() {
+        Context context = getContext();
+        String path = context.getFilesDir().toString();
+        Log.d("Files", "Path: " + path);
+        java.io.File directory = new java.io.File(path);
+        java.io.File[] files = directory.listFiles();
+        Log.d("Files", "Size: " + files.length);
+        for (int i = 0; i < files.length; i++) {
+            Log.d("Files", "FileName:" + files[i].getName());
+        }
+        return files;
+    }
+
+    public ArrayList<File> tomFile(java.io.File[] javaFiles) {
+        ArrayList<File> mFile = new ArrayList<File>();
+        for (int i = 0; i < javaFiles.length; i++) {
+            String dsType = null;
+            String dsDateModified = null;
+            com.example.datastructurevisualizer.File file = new com.example.datastructurevisualizer.File();
+            Log.d("Files", "FileName:" + javaFiles[i].getName());
+            try {
+                FileReader fileReader = new FileReader(javaFiles[i]);
+                BufferedReader bufferedReader = new BufferedReader(fileReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    stringBuilder.append(line).append("\n");
+                    line = bufferedReader.readLine();
+                }
+                bufferedReader.close();
+                // This response will have Json Format String
+                String response = stringBuilder.toString();
+
+                //parse string to JSONObject
+                JSONObject obj = new JSONObject(response);
+                Log.d("JSON obj", obj.toString());
+
+                //get json array of values from the object
+                JSONArray array = obj.optJSONArray("Values");
+                Log.d("Files", "FileName:" + javaFiles[i].getName());
+
+                if(obj.optString("Type") != null) {
+                    dsType = obj.getString("Type");
+                }
+                else{
+                    Log.d("Null Files type", "FileName:" + javaFiles[i].getName());
+                }
+                if(obj.optString("Date Modified") != null) {
+                    dsDateModified = obj.optString("Date Modified");
+                }
+                else{
+                    Log.d("Null date", "FileName:" + javaFiles[i].getName());
+                }
+
+                // Deal with the case of a non-array value.
+                if (array == null) {
+                    return null;
+                }
+                //add all values to arraylist arr
+                ArrayList<Integer> arr = new ArrayList<Integer>();
+                for (int j = 0; j < array.length(); ++j) {
+                    arr.add(array.optInt(j));
+                }
+                //set array to file
+                file.setValues(arr);
+
+                //set data structure type to file
+                if(dsType != null){
+                    file.setStructureType(dsType);
+                }
+
+                //set date modified to file
+                if(dsDateModified != null){
+                    file.setDate(dsDateModified);
+                }
+
+                //set filename to dsvisualizer file
+                file.setFileName(javaFiles[i].getName());
+
+            } catch (IOException | JSONException e) {
+                Log.e("Exception", "File read failed: " + e.toString());
+            }
+
+            //now all fields of file are filled, add that to file arraylist
+            mFile.add(file);
+
+        }
+        return mFile;
     }
 }
