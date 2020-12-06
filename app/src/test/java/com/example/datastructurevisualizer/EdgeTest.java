dd226474bc57105587f3e199450b13417d3560d3
Sam <sgronseth@wisc.edu>
2020-12-06 13:14:48 -0600

    update edge test parameters

--- app/src/test/java/com/example/datastructurevisualizer/EdgeTest.java
+++ app/src/test/java/com/example/datastructurevisualizer/EdgeTest.java
@@ -7,22 +7,22 @@ public class EdgeTest extends TestCase {
     private Node sNode = new Node(1, 2);
     private Node fNode = new Node(2, 2);
     private int weight = 2;
-    private Edge edge = new Edge(sNode, fNode, weight);
+    private Edge edge = new Edge(sNode, fNode, weight, false);
 
     public void testCompareTo_lessWeight() {
-        Edge compEdge = new Edge(fNode, sNode, 3);
+        Edge compEdge = new Edge(fNode, sNode, 3, false);
 
         assertEquals(-1, edge.compareTo(compEdge));
     }
 
     public void testCompareTo_moreWeight() {
-        Edge compEdge = new Edge(fNode, sNode, 1);
+        Edge compEdge = new Edge(fNode, sNode, 1, false);
 
         assertEquals(1, edge.compareTo(compEdge));
     }
 
     public void testCompareTo_sameWeight() {
-        Edge compEdge = new Edge(fNode, sNode, 2);
+        Edge compEdge = new Edge(fNode, sNode, 2, false);
 
         assertEquals(0, edge.compareTo(compEdge));
     }
