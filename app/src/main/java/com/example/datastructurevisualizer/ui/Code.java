16bd2a9dd05ecb9566af970b64f1fc073ea1e0b5
Allison Winter-Reed <38668004+allisonwinter-reed@users.noreply.github.com>
2020-12-07 15:26:29 -0600

    Code UI comments

--- app/src/main/java/com/example/datastructurevisualizer/ui/Code.java
+++ app/src/main/java/com/example/datastructurevisualizer/ui/Code.java
@@ -13,10 +13,17 @@ import androidx.fragment.app.Fragment;
 import com.example.datastructurevisualizer.MainActivity;
 import com.example.datastructurevisualizer.R;
 
+/**
+ * This class will control the information displayed on the 'Code' tab of the information page.
+ */
 public class Code extends Fragment {
     private String dataStructureType;
     private Button visualize;
 
+    /**
+     * Constructuor which sets the data structure type of the code page to be displayed
+     * @param dataStructureType
+     */
     public Code(String dataStructureType) {
         this.dataStructureType = dataStructureType;
     }
@@ -24,9 +31,13 @@ public class Code extends Fragment {
 
     @Nullable
     @Override
+    /**
+     * This method creates the view of the code tab based on the data structure type.
+     */
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view;
 
+        //Different layout files exist for the different data structure types
         switch (dataStructureType) {
             case "Binary Search Tree":
                 view = inflater.inflate(R.layout.code_bst, container, false);
@@ -73,6 +84,7 @@ public class Code extends Fragment {
 
                 break;
             default:
+                //Used as an error page early on when some of the data structure pages did not yet have layouts
                 view = inflater.inflate(R.layout.error_page, container, false);
                 Button home = view.findViewById(R.id.home_button);
                 home.setOnClickListener(new View.OnClickListener() {
@@ -82,7 +94,7 @@ public class Code extends Fragment {
                     }
                 });
         }
-
+        //returns the view created above
         return view;
     }
 }
